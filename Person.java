ee6476433f8162a1414a9cd6755935c5e68de39a
Vaisakh <260074834+vaisakh-collab@users.noreply.github.com>
2026-08-24 14:17:04 +0530

    Add Person.java

---
+++ Person.java
@@ -0,0 +1,15 @@
+public class Person{
+    private String name;
+    private int age;
+    private String phone;
+    private String address;
+    private String email;
+
+    public Person(String name, int age, String phone, String address, String email){
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.address = address;
+        this.email = email;
+    }
+}
