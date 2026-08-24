ee4f32bcf4632af5d997b4d35e133aef0d9e0761
Vaisakh <260074834+vaisakh-collab@users.noreply.github.com>
2026-08-25 01:03:43 +0530

    Appointment attributes

--- Appointment.java
+++ Appointment.java
@@ -1,3 +1,7 @@
 public class Appointment{
-    private int appointmentId
+    private int appointmentId;
+    private Patient patient;
+    private Doctor doctor;
+    private LocalDate date;
+    private LocalTime time;
 }
