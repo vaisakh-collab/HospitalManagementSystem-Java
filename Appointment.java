5637b35b5c2fce95f852cfe29e87385ae9320e95
Vaisakh <260074834+vaisakh-collab@users.noreply.github.com>
2026-08-25 15:22:48 +0530

    "Update Appointment.java"

--- Appointment.java
+++ Appointment.java
@@ -4,4 +4,26 @@ public class Appointment{
     private Doctor doctor;
     private LocalDate date;
     private LocalTime time;
+    private String status; //"Scheduled", "Completed", "Cancelled"
+
+    private static int nextId = 1; //keep track of AppointmentId to create an id each time constructor is called
+
+    //constructor
+
+    public Appointment(Patient patient, Doctor doctor, LocalDate date, LocalTime time){
+
+        //this.appointmentId = "A" + nextId++;       // create appointmentId as A1, A2, ...
+
+        this.appointmentId = String.format("A%03d", nextId++)     // padding zeroes till 3 digits as A001, A002, ...
+
+        this.patient = patient;
+        this.doctor = doctor;
+        this.date = date;
+        this.time = time;
+        this.status = "Scheduled"
+    }
+
+    //methods
+
+    
 }
