c915f3d3f9e0fbb264be1cea3a3e4b0f945e2ef9
nivedck52 <nivedck52@gmail.com>
2026-08-28 10:37:32 +0530

    Update Patient.java

--- Patient.java
+++ Patient.java
@@ -2,4 +2,26 @@ import java.util.List;
 public class Patient extends Person {
   private string patientId;
   private static int nextId = 1;
+  public Patient(String name, int age, String phone, String address, String email){
+    super(name, age, phone, address, email);
+    this.patienId = "P" + nextId;
+    nextId++;
+  }
+
+  public String getPatientId(){
+    return this.patientId;
+  }
+
+  public List<Consultation> viewMedicalHistory(){
+    return null;
+  }
+
+  public List<Appointment> viewAppointments(){
+    return null;
+  }
+
+  public List<Prescription> viewPrescriptions(){
+    return null;
+  }
+}
   
