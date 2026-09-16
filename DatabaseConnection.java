ab7428cd1fb0d000d076a7e618b7668da42914ef
Vaisakh <260074834+vaisakh-collab@users.noreply.github.com>
2026-09-16 17:09:14 +0530

    gitignore .proprties. Did DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -2,12 +2,28 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.util.Properties;
+
+
 public class DatabaseConnection {
 
     private static final String URL = "jdbc:mysql://localhost:3306/hospital";
-    private static final String USER = "root";
 
-    public static Connection getConnection(String password) throws SQLException {
-        return DriverManager.getConnection(URL, USER, password);
+    public static Connection getConnection() throws SQLException, IOException {
+
+        Properties properties = new Properties();
+
+        try (FileInputStream file = new FileInputStream("config.properties")) {
+            
+            properties.load(file);
+        }
+
+        String user = properties.getProperty("DB_USER");
+        String password = properties.getProperty("DB_PASSWORD");
+
+        return DriverManager.getConnection(URL, user, password);
     }
 }
